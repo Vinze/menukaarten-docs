c28efa6b725e81e04ea002ee1898925ac1531f14
Douwe de Haan <douwedehaan@live.nl>
2013-01-08 15:52:38 +0100

    disabled links in syntax plain

--- assets/js/SyntaxHighlighter_settings.js
+++ assets/js/SyntaxHighlighter_settings.js
@@ -1,3 +1,9 @@
 SyntaxHighlighter.defaults['gutter'] = false;
 SyntaxHighlighter.defaults['toolbar'] = false;
 SyntaxHighlighter.all();
+
+$('.syntaxhighlighter.plain a').click(function(){
+	e.preventDefault();
+	
+	return false;
+});
